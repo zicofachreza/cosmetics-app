60336415588e1c541077c32dbefbc2af3ff63639
zicofachreza <zico.fachreza@gmail.com>
2026-09-23 16:10:40 +0700

    update code

--- src/lib/midtrans.ts
+++ src/lib/midtrans.ts
@@ -1,13 +1,13 @@
 import midtransClient from 'midtrans-client'
 
 export const snap = new midtransClient.Snap({
-    isProduction: true, // Ubah ke true nanti kalau sudah live
+    isProduction: false, // Ubah ke true nanti kalau sudah live
     serverKey: process.env.MIDTRANS_SERVER_KEY!,
     clientKey: process.env.MIDTRANS_CLIENT_KEY!,
 })
 
 export const core = new midtransClient.CoreApi({
-    isProduction: true, // Ubah ke true nanti kalau sudah live
+    isProduction: false, // Ubah ke true nanti kalau sudah live
     serverKey: process.env.MIDTRANS_SERVER_KEY!,
     clientKey: process.env.MIDTRANS_CLIENT_KEY!,
 })
